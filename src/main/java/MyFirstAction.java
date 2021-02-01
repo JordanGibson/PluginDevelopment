47191a1cf8a7782bdf450ede161608e0a7126dd9
Jordan Gibson <34385202+JordanGibson@users.noreply.github.com>
2021-02-01 01:51:58 +0000

    Modifying MyFirstAction.java

--- src/main/java/MyFirstAction.java
+++ src/main/java/MyFirstAction.java
@@ -5,12 +5,21 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.notification.Notification;
 
+import java.util.Objects;
+
 public class MyFirstAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         new Notification(Notifications.SYSTEM_MESSAGES_GROUP_ID,
-                "My first notification",
-                "Hello world!", NotificationType.INFORMATION)
+                String.format("The current project is %s", e.getProject().getName()),
+                String.format("The current working directory is %s", e.getProject().getProjectFilePath()),
+                NotificationType.INFORMATION)
                 .notify(null);
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        super.update(e);
+        e.getPresentation().setEnabled(Objects.nonNull(e.getProject()));
     }
 }
